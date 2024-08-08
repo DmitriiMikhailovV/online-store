44e99046e41bf4df61b84c8a447c7131050d9e4b
Mikhailov <mihaylovdmitriy3@gmail.com>
2024-08-08 23:56:28 +0200

    added Home Page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    <h1>Main Page</h1>
+  <div
+    class="h-screen bg-cover bg-center flex justify-center items-center text-white background-image"
+  >
+    <div
+      class="absolute bottom-4 right-4 text-2xl text-black text-right drop-shadow"
+    >
+      Created by:<br />
+      <a
+        href="https://www.linkedin.com/in/dmitrii-mikhailov-12101989/"
+        target="_blank"
+        class="text-black"
+        ><i class="fa-brands fa-linkedin mr-2"></i>Dmitrii Mikhailov</a
+      >
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup></script>
 
-export default defineComponent({
-  name: 'HomeView',
-})
-</script>
+<style scoped>
+.background-image {
+  @apply bg-[url('@/assets/main_image.png')];
+}
+</style>
